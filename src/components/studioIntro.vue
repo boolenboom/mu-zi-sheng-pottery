86f3e00a7fa121335b64371b35b1b330636e6ebd
boolenboom <alan93072@gmail.com>
2021-06-01 13:49:40 +0800

    開發工作室介紹組件

---
+++ src/components/studioIntro.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="section studioIntro">
+        <div class='background'>
+            <div class="comp">
+                <div class='Rect-01'>
+                    
+                </div>
+                <div class='Rect-02'>
+                    
+                </div>
+                <div class='Rect-03'>
+                    
+                </div>
+                <div class='Rect-04'>
+                    
+                </div>
+                <div class='Rect-05'>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'studioIntro',
+    data(){
+        return null;
+    },
+}
+</script>
+<style lang="scss" scoped>
+.studioIntro {
+    height: auto;
+    .comp {
+        position: absolute;
+        width: 78.75%;
+        height: 140%;
+        transform: rotate(45deg) translate(14%, -17.9%);
+        z-index: 1;
+        [class*='Rect-'] {
+            background-color: var(--BG-color);
+            // outline: 1px solid #000;
+            overflow: hidden;
+            img {
+                transform: rotate(-45deg) scale(2);
+            }
+        }
+        [class*='-01'] {
+            top: -25%;
+            width: 150%;
+            height: 50%;
+            img {
+                position: relative;
+                top: 15%;
+                left: -3%;
+            }
+        }
+        [class*='-02'] {
+            top: 25%;
+            width: 25%;
+            height: 25%;
+            img {
+                position: relative;
+                top: -5%;
+            }
+        }
+        [class*='-03'] {
+            top: 25%;
+            left: 50%;
+            width: 100%;
+            height: 25%;
+            img {
+                position: relative;
+                top: -15%;
+                left: -10%;
+            }
+        }
+        [class*='-04'] {
+            bottom: 25%;
+            width: 50%;
+            height: 25%;
+            img {
+                position: relative;
+                top: 80%;
+                left: -50%;
+            }
+        }
+        [class*='-05'] {
+            bottom: 0;
+            left: 50%;
+            width: 25%;
+            height: 50%;
+            img {
+                position: relative;
+                top: -15%;
+                left: -100%;
+            }
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,3 +16,43 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+$pc:1440px;
+$table:768px;
+$mobile:360px;
+body {
+    height: 100%;
+}
+
+* {
+    padding: 0;
+    margin: 0;
+    text-decoration: none;
+}
+
+ :root {
+    --BG-color: #C4C4C4;
+}
+
+.section {
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+    text-align: center;
+}
+
+.background {
+    position: relative;
+    margin: 0 auto;
+    height: 100vh;
+    width: 177.778vh;
+    @media (max-width: 177vh) {
+        margin: 0 calc((100% - 177.778vh) / 2);
+    }
+}
+
+[class*='Rect-'] {
+    position: absolute;
+    background-color: #fff;
+}
+</style>
