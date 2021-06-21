85840dfffd0298204acbaf350205efe9fc9d145a
boolenboom <alan93072@gmail.com>
2021-06-21 18:25:09 +0800

    版面完成

--- src/App.vue
+++ src/App.vue
@@ -32,11 +32,20 @@ html,body{
 
 #nav {
   padding: 30px;
-
+  position: fixed;
+  z-index: 20;
+  width: calc(100% - 60px);
+  display: flex;
+  flex-flow: row nowrap;
+  font-size: 2rem;
   a {
+    flex: 1 0 0;
     font-weight: bold;
     color: #2c3e50;
 
+    &:nth-child(1){
+      margin-right: 80%;
+    }
     &.router-link-exact-active {
       color: #42b983;
     }
