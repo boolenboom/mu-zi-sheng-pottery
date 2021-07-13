3b5db1be2b84c8decb88f7d43cb53c3cb2660015
boolenboom <alan93072@gmail.com>
2021-07-13 16:53:32 +0800

    匯入carousel & 完成初始樣式

---
+++ src/components/carousel.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="carousel" style="`--main-color:#fff`;">
+        <div class="warpper">
+            <div class="title">{{sectiontitle}}</div>
+            <ul class="contents">
+                <li v-for="i of quantity" class="item" :key="i">
+                    <div class="pic">
+                        <!-- <img src="#" alt="" class="pic"> -->
+                    </div>
+                    <p class="itemname">Card name</p>
+                </li>
+            </ul>
+            <a href="#" class="button">All</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'carousel',
+    props:{
+        sectiontitle:{type:String,default:'Intro'},
+        quantity:{type:Number,default:3},
+        countdown:{type:Number,default:3}
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.carousel{
+    position: relative;
+    z-index: 30;
+    width: 100%;
+    font-size: 1.5rem;
+    overflow-x: hidden;
+    .title{
+        top: 10%;
+        font-size: 12rem;
+        writing-mode: vertical-lr;
+        position: absolute;
+    }
+    .button{
+        font-size: 4rem;
+        position: absolute;
+        top: 10%;
+        right: 0;
+    }
+    .contents{
+        margin-top: 10rem;
+        width: 100%;
+        display: flex;
+        flex-flow: row nowrap;
+        margin-left: calc(20% + 5% - 16px);
+        .item{
+            list-style-type: none;
+            flex:50% 0 0;
+            // height: calc(20vh + 300px);
+            outline: 1px solid #000;
+            padding: 16px;
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
+            justify-content: center;
+            .pic{
+                min-width: 192px;
+                width: 100%;
+                height: 600px;
+                border-radius: 12px;
+                background-color: var(--main-color);
+            }
+            .itmename{
+                width: 100%;
+                margin-top: 3rem;
+            }
+            &:not(:first-child){
+                margin-left: 20%;
+            }
+        }
+    }
+}
+.warpper{
+    width: 100%;
+}
+</style>
--- src/components/potteryList.vue
+++ src/components/potteryList.vue
@@ -1,53 +1,56 @@
 <template>
-    <section id="potteryList" class="section">
-        <div class='background'>
-            <div class="Rect-07"></div>
-            <div class="Rect-08"></div>
-            <div class="full"></div>
-        </div>
-    </section>
+  <section id="potteryList" class="section">
+    <div class="background">
+      <!-- <div class="Rect-07"></div> -->
+      <!-- <div class="Rect-08"></div> -->
+      <carousel />
+      <div class="full"></div>
+    </div>
+  </section>
 </template>
 <script>
+import carousel from './carousel.vue';
+
 export default {
-    name:'potteryList',
-}
+  name: "potteryList",
+  components:{
+    carousel,
+  }
+};
 </script>
 <style lang="scss" scoped>
 #potteryList {
-    [class*='-07'] {
-        width: 73.4375%;
-        height: 87.037%;
-        background-color: #fff;
-        transform: rotate(45deg) translate(54.6%, -42.4%);
+	overflow-y: hidden;
+  // [class*="-07"] {
+  //   width: 73.4375%;
+  //   height: 87.037%;
+  //   background-color: #fff;
+  //   transform: rotate(45deg) translate(54.6%, -42.4%);
+  // }
+  // [class*="-08"] {
+  //   top: 0;
+  //   left: 0;
+  //   @media (min-width: 177vh) {
+  //     left: calc((100vw - 177.778vh) / -2);
+  //     width: 100vw;
+  //   }
+  //   width: 100%;
+  //   height: 60%;
+  //   transform: translateY(26%);
+	// 	background-color: transparent;
+  // }
+  .full {
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    @media (min-width: 177vh) {
+      left: calc((100vw - 177.778vh) / -2);
+      width: 100vw;
     }
-    [class*='-08'] {
-        top: 0;
-        left: 0;
-        @media (min-width: 177vh) {
-            left: calc((100vw - 177.778vh) / -2);
-            width: 100vw;
-        }
-        width: 100%;
-        height: 60%;
-        transform: translateY(26%);
-        background-color: var(--BG-color);
-    }
-    .full {
-        position: absolute;
-        z-index: -1;
-        top: 0;
-        left: 0;
-        @media (min-width: 177vh) {
-            left: calc((100vw - 177.778vh) / -2);
-            width: 100vw;
-        }
-        width: 100%;
-        height: 120%;
-            background-image: url('../assets/BG.jpg');
-            background-position: top center;
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-attachment: fixed;
-    }
+    width: 100%;
+    height: 120%;
+    background: #85b8cb;
+  }
 }
 </style>
