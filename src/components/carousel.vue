3f8fe94abcaf0582ca1544bc5f633e382ccb80bf
boolenboom <alan93072@gmail.com>
2021-07-14 10:49:50 +0800

    控制部分完成

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="carousel" style="`--main-color:#fff`;">
-        <div class="warpper">
+    <div class="carousel">
+        <div class="warpper" @mousedown.prevent="controller($event)" @mousemove.prevent="controller($event)" @mouseup.prevent="controller($event)">
             <div class="title">{{sectiontitle}}</div>
-            <ul class="contents">
-                <li v-for="i of quantity" class="item" :key="i">
+            <ul class="contents" :data-offset='offset' :style="`--offset:${offset}px;`">
+                <li v-for="i of quantity" class="item" :key="i" draggable="false">
                     <div class="pic">
                         <!-- <img src="#" alt="" class="pic"> -->
                     </div>
@@ -16,12 +16,53 @@
 </template>
 
 <script>
+function createState(){
+    let on = false;
+    let offset = 0;
+    let startpoint = 0;
+    let currentoffset = 0;
+    return {
+        mousedown:function(val){
+            on = true;
+            startpoint = val;
+        },
+        mousemove:function(val){
+            if(on) currentoffset = val - startpoint;
+        },
+        mouseup:function(val){
+            on = false;
+            offset += currentoffset;
+            currentoffset = 0;
+            console.log('slide event end ,end point:',val);
+            console.log(this.getstate());
+        },
+        getstate:function(){
+            return [on,offset,startpoint,currentoffset];
+        },
+        getoffset:function(){
+            return offset + currentoffset;
+        }
+    }
+}
 export default {
     name:'carousel',
     props:{
         sectiontitle:{type:String,default:'Intro'},
         quantity:{type:Number,default:3},
         countdown:{type:Number,default:3}
+    },
+    data(){
+        return{
+            UIcontroller:createState(),
+            offset:0
+        }
+    },
+    methods:{
+        controller(e){
+            // console.log(e,this.moveOffset);
+            this.UIcontroller[e.type](e.x);
+            this.offset = this.UIcontroller.getoffset();
+        }
     }
 };
 </script>
@@ -51,7 +92,7 @@ export default {
         width: 100%;
         display: flex;
         flex-flow: row nowrap;
-        margin-left: calc(20% + 5% - 16px);
+        margin-left: clamp(0px, calc(20% + 5% - 16px + var(--offset)), calc(20% + 5% - 16px)) ;
         .item{
             list-style-type: none;
             flex:50% 0 0;
