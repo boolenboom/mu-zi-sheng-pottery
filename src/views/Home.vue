d7591615b67c248bab6d0d1a6d595bc2d6784240
boolenboom <alan93072@gmail.com>
2021-08-04 13:17:57 +0800

    bugFix-更動視窗高度時滾輪數值錯誤 & 滾輪邏輯整理

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id='Home' class="home" @wheel="scrollhandler($event)" @transitionend.self='snap($event)' :style="`transform:translateY(${pageoffset}px);`">
+  <div id='Home' class="home" 
+  @wheel="scrollhandler($event)" 
+  @transitionend.self='snap($event)' 
+  :style="`transform:translateY(${pageoffset}px);`">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <keyVision :viewon='currView'/>
     <studiointro :viewon='currView' :offset='pageoffset'/>
@@ -14,6 +17,13 @@ import studiointro from "@/components/studioIntro.vue";
 import potterylist from '@/components/potteryList.vue';
 
 let scrollratio = 0.2;
+let clamp = function(val,min,max){
+      return Math.min(max,Math.max(val,min));
+    },
+    checkCurrheight = function(cacheHeight){
+      if(window.innerHeight === cacheHeight)return cacheHeight;
+      return window.innerHeight;
+    };
 
 export default {
   name: "Home",
@@ -26,36 +36,40 @@ export default {
     return{
       pageoffset:0,
       childNum:0,
-      viewheight:window.innerHeight
+      viewheight:0,
     }
   },
   methods:{
     scrollhandler(e){
+      this.viewheight = checkCurrheight(this.viewheight);
       let delta = e.deltaY,
           viewH = this.viewheight;
       this.pageoffset -= delta / 100 * viewH * scrollratio;
-      this.pageoffset = this.pageoffset > 0 ? 
-        0 : -this.pageoffset > viewH* (this.childNum - 1) ?
-        -viewH* (this.childNum - 1) : this.pageoffset;//限制範圍
-      
+      this.pageoffset = clamp(this.pageoffset, -viewH * (this.childNum - 1), 0);
     },
     snap(e){
       console.log(e.type);
-      // let viewH = this.viewheight;
-      // if(this.pageoffset % viewH=== 0)return 0;
-      // this.pageoffset = Math.round(this.pageoffset / viewH) * viewH;
+      let viewH = this.viewheight;
+      if(this.pageoffset % viewH=== 0)return 0;
+      this.pageoffset = Math.round(this.pageoffset / viewH) * viewH;
     }
   },
   computed:{
     currView:function(){
-      let viewscope = [0.5*this.viewheight,1.5*this.viewheight],
-      curr = viewscope[0] > -this.pageoffset ? 1 : viewscope[1] > -this.pageoffset ? 2 : 3;
+      let viewscope = this.viewheight,
+      count = 1,
+      curr = 0;
+      while(-this.pageoffset > viewscope * (count - 0.5)){
+        count++;
+      }
+      curr = count;
       return curr;
     }
   },
   mounted(){
     const parent = document.querySelector('.home');
     this.childNum = parent.childNodes.length;
+    this.viewheight = window.innerHeight;
   }
 };
 </script>
@@ -63,7 +77,7 @@ export default {
 <style lang="scss">
 
 .home{
-  transition: transform 1.5s cubic-bezier(.2,.4,.7,.9);
+  transition: transform 1s cubic-bezier(.2,.4,.7,.9);
 }
 
 [class*='Rect-'] {
