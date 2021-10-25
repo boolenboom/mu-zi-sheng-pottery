ca89dd084c7bdd21a198813dd24a46d87014a3a3
boolenboom <alan93072@gmail.com>
2021-10-25 13:32:02 +0800

    檔名更改about->works

--- src/router/index.js
+++ src/router/index.js
@@ -12,14 +12,22 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/works",
+    name: "works",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Works.vue"),
+    // children:[{
+    //   path: "/get",
+    //   component: worksList,
+    // }],
   },
+  {
+    path: "*",
+    redirect: "/",
+  }
 ];
 
 const router = new VueRouter({
